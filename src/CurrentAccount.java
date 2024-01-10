6353373297febb8f5ed247afc30eec7915d7cc29
NeoM404 <mthi45martin@gmail.com>
2024-01-10 10:27:45 +0200

    Implemented withdrawal in CurrentAccount

--- src/CurrentAccount.java
+++ src/CurrentAccount.java
@@ -1,2 +1,21 @@
-public class CurrentAccount {
+ class CurrentAccount extends Account{
+
+    private double overdraftLimit;
+
+    public CurrentAccount(String accountNumber, double initialBalance, double overdraftLimit){
+        super(accountNumber, initialBalance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+     public void withdraw(double amount){
+        if (getBalance() + this.overdraftLimit >= amount)
+        {
+            super.withdraw(amount);
+            System.out.println("withdrawal successful. New balance: R" + getBalance());
+        } else {
+            System.out.println("Exceeds overdraft limit. Withdrawal denied");
+        }
+    }
+
 }
