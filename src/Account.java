646463073f8cdebfaa60f7f0255d354202c8b86c
NeoM404 <mthi45martin@gmail.com>
2024-01-10 09:56:46 +0200

    Added an abstract class Account

---
+++ src/Account.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+abstract class Account {
+    private String accountNumber;
+    private double balance;
+
+    private Account(String accountNumber, double initialBalance)
+    {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+
+    public void deposit(double amount)
+    {
+        balance += amount;
+    }
+
+    public abstract void withdraw(double amount);
+
+    public void displayBalance()
+    {
+        System.out.println("Account Number: " + this.accountNumber);
+        System.out.println("Balance: R" + this.balance);
+    }
+}
